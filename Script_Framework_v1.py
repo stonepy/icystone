c8e9e9275cb3b0266eb095c7f24869b3f05ff221
stonepy <13210700020@fudan.edu.cn>
2017-03-15 13:31:26 +0800

    hello world

--- Script_Framework_v1.py
+++ Script_Framework_v1.py
@@ -1,11 +1,11 @@
 """
 - Information --------------------------------------------------------------------
- Name         :
- Description  :
- Author       :   Hwx
- Version      :   V1
- Dev Env      :   Red Hat 4.8.5-11/Ubuntu16.04 LTS, Python3.5.3, virtualenv15.1.0
- Finish Date  :   2017-03-14
+ Name         : Script_Framework
+ Description  : Build new project with it
+ Author       : Hwx
+ Version      : V1
+ Dev Env      : Red Hat 4.8.5-11/Ubuntu16.04 LTS, Python3.5.3, virtualenv15.1.0
+ Finish Date  : 2017-03-14
 -----------------------------------------------------------------------------------
 """
 
