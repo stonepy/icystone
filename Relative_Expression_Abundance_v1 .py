a3b8d4400d49a43073cc386fb0ffc5ff39951122
stonepy <13210700020@fudan.edu.cn>
2017-03-15 17:35:29 +0800

    hello world

--- Relative_Expression_Abundance_v1 .py
+++ Relative_Expression_Abundance_v1 .py
@@ -14,7 +14,7 @@
 import pandas as pd
 
 
-def main():
+def REA():
     sampleType  = "tRNA_id"
     path_expr   = "/home/daniel/PycharmProjects/fishbone/files/miRNA_tRNA/tRNA.expression.xls"
     path_output = "/home/daniel/PycharmProjects/fishbone/files/output/tRNA.REA.xls"
@@ -30,19 +30,63 @@ def main():
     # merge into original sheet
     df_exprMerged = pd.concat([df_expr, df_exprSum]).set_index(sampleType)
 
-    # Calculate the
+    # Calculate the REA
     for col in df_exprMerged:
         Sum = df_exprMerged.ix[["Total_Expression"], col]
         df_exprMerged[col] = df_exprMerged[col].apply(lambda x: x / Sum)
 
-
+    # Output result
     df_exprMerged.to_csv(path_output, index="miRNA")
 
 
 
+def Add_fastaSeq():
+    sampleType  = "tRNA_id"
+    path_fasta  = "/home/daniel/PycharmProjects/fishbone/files/miRNA_tRNA/hg19-tRNAs.fa"
+    path_REA    = "/home/daniel/PycharmProjects/fishbone/files/output/tRNA.REA.xls"
+    path_output = "/home/daniel/PycharmProjects/fishbone/files/output/tRNA.REAseq.xls"
+
+    # Read REA data sheet
+    df_REA = pd.read_csv(path_REA)
+
+    # Read reference fasta file
+    with open(path_fasta, "r") as fas:
+        fa_list = []
+        for fa in fas:
+            fa_list.append(fa)
+
+    # Select sequences by tRNA ID
+    df_fas_list = []
+    for index, row in df_REA.iterrows():
+
+        fa_seq = ""
+        for fa in fa_list:
+
+            # When ID line, stop collect
+            if fa.startswith(">"):
+                collect_switch = False
+
+            #  Find corresponding sequence
+            if row[0] in fa:
+                collect_switch = True
+                continue
+
+            # When corresponding sequence found,
+            if collect_switch:
+                fa_seq += fa.strip()
+
+        # Put all selected sequences in a list
+        df_fas_list.append(fa_seq)
+
+    # Convert to DataFrame and merge into REA sheet
+    df_fas = pd.DataFrame(df_fas_list, columns=["sequence"])
+    df_REAseq = pd.concat([df_REA, df_fas], axis=1)
+
+    df_REAseq.to_csv(path_output, index=None)
 
 
 
 if __name__ == "__main__":
 
-    main()
+    REA()
+    Add_fastaSeq()
