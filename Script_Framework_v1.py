6e58cedcc7eb1e02e21a3382bc2ef1e91e6c032f
stonepy <13210700020@fudan.edu.cn>
2017-03-22 17:23:01 +0800

    hello world

--- Script_Framework_v1.py
+++ Script_Framework_v1.py
@@ -20,8 +20,8 @@ class main:
     def __init__(self):
         pass
 
-    def function(self):
-        pass
+    def function(self, hg):
+        print(hg)
 
 
 
@@ -65,7 +65,7 @@ class management:
         # No argumnet passed
         if args== None :
             p = Pool(process_num)
-            for i in process_num:
+            for i in range(process_num):
                 p.apply_async(function_name)
             p.close()
             p.join()
@@ -73,7 +73,7 @@ class management:
         # Only one argument passed
         elif isinstance(args, list) == False :
             p = Pool(process_num)
-            for i in process_num:
+            for i in range(process_num):
                 p.apply_async(function_name, args=(args,))
             p.close()
             p.join()
@@ -97,8 +97,17 @@ class management:
 
 if __name__ == "__main__":
 
+    # Parameter Section
+
+    hg = "helloe world"
     words_list = ['default', 'arguments', 'are', 'evaluated', 'once', 'when']
-    function_name = management().testFunction
     process_num = 3
 
-    management().multiP(function_name, words_list, process_num)
+
+    # Instantiation
+    function_name = management().testFunction
+
+
+
+    # Execution part
+    management().multiP(main().function, hg, process_num)
