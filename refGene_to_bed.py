2306e137447e35ceb97f7310ebd681fbde163a2a
IcyStone <13210700020@fudan.edu.cn>
2017-04-10 12:21:56 +0800

    Update refGene_to_bed.py

--- refGene_to_bed.py
+++ refGene_to_bed.py
@@ -1,48 +1,59 @@
 """
 - Information --------------------------------------------------------------------
  Name         : refGene_to_bed
- Description  : Transform '*_refGene.txt' to 'TAIR10.bed'
+ Description  : Transform '*_refGene.txt' to '*.bed', '*'stands for species, specific for SNP pipline
  Formulation  : None
  Author       : Hwx
- Version      : V1
+ Version      : V3
  Dev Env      : Red Hat 4.8.5-11/Ubuntu16.04 LTS/Windows10 Home CN, Python3.5.3, virtualenv15.1.0
- Finish Date  : 2017-03-24
+ Finish Date  : 2017-04-10
 -----------------------------------------------------------------------------------
 """
 
 
+import sys
 
+# Test path
+path_refGene   = "mm10_refGene_old.txt"
+path_BedOutput = "mm10_new.bed"
+
+# Parse arguments
+args = sys.argv
+try:
+    path_refGene   = args[1]
+    path_BedOutput = args[2]
+
+except:
+    print("""
+    Usage:
+        python refGene_to_bed.py <refGene_input> <BEDoutput_output>
+    """)
+    exit()
+
+# Import pandas here for faster launching
 import pandas as pd
 
+# Read '*_refGene.txt' as DataFrame
+df_refGene = pd.read_table(path_refGene, header=None)
 
-path_refGene   = "TAIR10_refGene.txt"
-path_BedOutput = "TAIR10.bed"
-
-df_refGene   = pd.read_table(path_refGene)
-
+# Seperate information and output to '*.bed'
 with open(path_BedOutput, "w") as bed:
+    # Sort by chromosome
+    df_refGene = df_refGene.sort_values(by=2, ascending=True)
 
+    # Obtain information
     for index, row in df_refGene.iterrows():
-        iso   = row[1]
-        chr   = row[2]
-        ori   = row[3]
-        start = row[9].split(",")[:-1]
-        end   = row[10].split(",")[:-1]
+        iso   = row[1]                          # Isoform name
+        chr   = row[2].strip("chr")             # Chromosome, only need number
+        ori   = row[3]                          # Orientation
+        start = row[9].split(",")[:-1]          # Start site
+        end   = row[10].split(",")[:-1]         # End site
 
-        n = 0
+        # Output the treated items
+        n = 0               # Count of isoform
         for coord in zip(start, end):
-            print(coord)
+
             n += 1
-            iso_n = iso + str(n)
-            l = "\t".join([str(chr), str(coord[0]), str(coord[1]), str(ori), iso_n]) + "\n"
+            iso_n = iso + "." + str(n)      # Use id of isoform and number to identify name, must convert number to string
+            l = "\t".join([str(chr), str(coord[0]), str(coord[1]), str(ori), iso_n]) + "\n"     # Merge all items into a line
             bed.write(l)
-
-
-
-
-
-# print(df_refGene)
-
-
-
-
