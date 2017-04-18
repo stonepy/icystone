ef30034839b28ed2d8948df6dfb4c023559d316d
IcyMount <13210700020@fudan.edu.cn>
2017-04-18 08:54:09 +0800

    Merge branch 'master' of https://github.com/stonepy/fishbone
    # Conflicts:
    #	Enterotyping Package/Example Data/MetaHIT_SangerSamples.genus.txt
    #	Enterotyping Package/Paper/Enterotypes of the human gut microbiome.pdf
    #	Enterotyping Package/Script/Enterotyping_PCoA_v1.R
    #	run_Timer.py

--- SNP_mRNA_Pipline/packages/process_manager.py
+++ SNP_mRNA_Pipline/packages/process_manager.py
@@ -1,4 +1,5 @@
-"""
+Description = """
+
 - Information --------------------------------------------------------------------
  Name         : ProcessManager_SNP_mRNA_Pipline
  Description  : Run modules with single or multiple processes automatically
@@ -7,11 +8,22 @@
  Dev Env      : Red Hat 4.8.5-11/Ubuntu16.04 LTS, Python3.5.3, virtualenv15.1.0
  Finish Date  : 2017-04-
 -----------------------------------------------------------------------------------
+
 """
+
+Usage = """
+
+        Usage:
+
+            python %s <WTS_CongfigPATH>
+
+
+""" % __file__
 
 
 
 from multiprocessing import Pool
+from subprocess      import call
 import time
 
 
@@ -21,59 +33,19 @@ class main:
     def __init__(self):
         pass
 
-    def testFunction(self, word):
+    def call_func(self, arg):
+        call(arg, shell=True)
 
-        def t():
-            print(word)
-            time.sleep(1)
-        t()
-
-
-    """
-
-        Note:
-
-        Apply multiple processes in different ways
-            1) no arg
-            2) one arg
-            3) more than one arg (list)
-
-
-        Usage:
-
-        This function accepts 2 positional and 1 optional args:
-            1) the name of the function
-            2) the arg(s)
-            3) optional, numbers of processes
-
-    """
-    def multiP(self, function_name, args=None, process_num=4):
+    def multiP(self, para_dict, call_func):
 
         start_t = time.time()
 
-        # No argumnet passed
-        if args== None :
-            p = Pool(process_num)
-            for i in range(process_num):
-                p.apply_async(function_name)
-            p.close()
-            p.join()
 
-        # Only one argument passed
-        elif isinstance(args, list) == False :
-            p = Pool(process_num)
-            for i in range(process_num):
-                p.apply_async(function_name, args=(args,))
-            p.close()
-            p.join()
-
-        # Mutiple arguments passed
-        elif isinstance(args, list) == True:
-            p = Pool(process_num)
-            for i in args:
-                p.apply_async(function_name, args=(i,))
-            p.close()
-            p.join()
+        p = Pool(para_dict["nProcess"])
+        for i in para_dict["args"]:
+            p.apply_async(call_func, args=(i,))
+        p.close()
+        p.join()
 
         total_t = time.time() - start_t
         print("Time concumed: %0.2fs" % total_t)
@@ -83,16 +55,16 @@ class main:
 if __name__ == "__main__":
 
     # Parameter Section
-
-    hg = "helloe world"
-    words_list = ['default', 'arguments', 'are', 'evaluated', 'once', 'when']
-    process_num = 3
+    para_dict = {
+        "nRun"      : None,
+        "nProcess"  : None,
+        "args"      : [],
+    }
 
 
     # Instantiation
-    function_name = management().testFunction
-
+    main = main()
 
 
     # Execution part
-    management().multiP(main().function, hg, process_num)
+    main.multiP(main.call_func)
