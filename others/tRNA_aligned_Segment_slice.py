e2c695c6a914eb750ecc3ee4bda1ee493dd6d440
stonepy <13210700020@fudan.edu.cn>
2017-05-24 14:43:09 +0800

    hello world

--- others/tRNA_aligned_Segment_slice.py
+++ others/tRNA_aligned_Segment_slice.py
@@ -1,9 +1,27 @@
-import time
+
+Info = {
+# _ Information ____________________________________________________________________
+
+    "__Name"         : "tRNA_aligned_Segment_slice",
+    "__Description"  : "Use for selecting the corresponding segments of tRNA reads\
+                        aligned to the reference sequences",
+
+    "__Author"       : "Hwx",
+    "__Version"      : "V0",
+    "__Dev Env"      : "Red Hat 4.8.5_11/Ubuntu16.04 LTS;Python3.5.3,virtualenv15.1.0",
+    "__Finish Date"  : "2017_05-24",
+# __________________________________________________________________________________
+
+}
+
+
+
+
 import sys
 
 
 try:
-    if len(sys.argv[1:]) == 3:
+    if len(sys.argv[1:])   == 3:
         reference_path, output_name, blastRes_path = sys.argv[1:]
 
     elif len(sys.argv[1:]) == 4:
@@ -26,7 +44,7 @@ except:
 
     try:
         blastRes_path   = "1_10000.blast.tsv"
-        bowtie2Res_path = "1_10000.blast.tsv"
+        bowtieRes_path  = "1_tmp.sam"
         reference_path  = "hg19-tRNAs.fa"
         output_name     = "aligned_segments"
 
@@ -47,50 +65,70 @@ def reference_seqMerge(reference_path):
             if l.startswith(">"):
                 if fa_tmp:
                     ref_dict[l] = fa_tmp
-                    fa_tmp = None
+                    fa_tmp      = None
 
             else:
                 try:
                     fa_tmp += l
-
                 except:
                     fa_tmp  = l
 
     return ref_dict
 
 
-def blast(ref_dict):
-    output_path = output_name + ".blast.trd"
-    blastRes    = open(blastRes_path, "r")
+def blast(blastRes_path, ref_dict):
+    output_path   = output_name + ".blast.trd"
+    blastRes      = open(blastRes_path, "r")
     blastRes_dict = {}
 
     for bl in blastRes:
-        bl_spl  = bl.strip().split("\t")
-        bl_id   = bl_spl[1]
+        bl_spl    = bl.strip().split("\t")
+        bl_id     = bl_spl[1]
         sbj_start = int(bl_spl[8])
         sbj_end   = int(bl_spl[9])
 
-        for seq_id in ref_dict.keys():
-            if bl_id in seq_id:
-                seq_tmp = ref_dict[seq_id]
-                faLen   = len(ref_dict[seq_id])
-
-                if sbj_start > sbj_end:
-                    sbj_start = faLen - sbj_start + 1
-                    sbj_end   = faLen - sbj_end + 1
-                    seq_align = seq_tmp[::-1][sbj_start:sbj_end][::-1]
-                else:
-                    seq_align = seq_tmp[sbj_start:sbj_end]
-
-                try:
-                    blastRes_dict[bl_id].append([sbj_start,sbj_end,seq_align])
-                except:
-                    blastRes_dict[bl_id] = []
-                    blastRes_dict[bl_id].append([sbj_start,sbj_end,seq_align])
+        ref_search(blastRes_dict, bl_id, sbj_start, sbj_end)
 
     blastRes.close()
     format_output(output_path, ref_dict, blastRes_dict)
 
+
+def bowtie(bowtieRes_path, ref_dict):
+    output_path    = output_name + ".bowtie.trd"
+    bowtieRes      = open(bowtieRes_path, "r")
+    bowtieRes_dict = {}
+
+    for bt in bowtieRes:
+        bt_spl    = bt.strip().split("\t")
+        bt_id     = bt_spl[2]
+        sbj_start = int(bt_spl[3])
+        sbj_end   = int(bt_spl[5].strip("M"))
+
+        ref_search(bowtieRes_dict, bt_id, sbj_start, sbj_end)
+
+    bowtieRes.close()
+    format_output(output_path, ref_dict, bowtieRes_dict)
+
+
+def ref_search(res_dict, res_id, sbj_start, sbj_end):
+
+    for seq_id in ref_dict.keys():
+        if res_id in seq_id:
+            seq_tmp = ref_dict[seq_id]
+            faLen   = len(ref_dict[seq_id])
+
+            if sbj_start > sbj_end:
+                sbj_start = faLen - sbj_start + 1
+                sbj_end   = faLen - sbj_end + 1
+                seq_align = seq_tmp[::-1][sbj_start:sbj_end][::-1]
+            else:
+                seq_align = seq_tmp[sbj_start:sbj_end]
+
+            try:
+                res_dict[res_id].append([sbj_start, sbj_end, seq_align])
+            except:
+                res_dict[res_id] = []
+                res_dict[res_id].append([sbj_start, sbj_end, seq_align])
 
 
 def format_output(output_path, Ref_dict, Res_dict):
@@ -107,7 +145,7 @@ def format_output(output_path, Ref_dict, Res_dict):
                 ref_seq = Ref_dict[seq_id]
 
                 if res_id in seq_id:
-                    refSeq_len = len(ref_seq)
+                    refSeq_len       = len(ref_seq)
                     out_dict[res_id] = [ref_seq+"\n"]
 
                     for info in Res_dict[res_id]:
@@ -133,82 +171,21 @@ def format_output(output_path, Ref_dict, Res_dict):
             for l in out_dict[id]:
 
                 if ref_line:
-                    x = idBlock_len - len("Reference Sequence:")
-                    outLine = "Reference Sequence:" + (space * x) + l
+                    n        = idBlock_len - len("Reference Sequence:")
+                    outLine  = "Reference Sequence:" + (space * n) + l
                     fm_out.write(outLine)
                     ref_line = False
                     continue
 
                 outLine = id + spaces + l
                 fm_out.write(outLine)
-                print(outLine)
 
-
-def bowtie():
-    with open(output_name+".bowtie2.fa", "w") as res_fa:
-
-        bowtie2Res = open(bowtie2Res_path, "r")
-
-        id_tmp  = None
-        seq_tmp = None
-
-        for bo in bowtie2Res:
-
-            bo_spl  = bo.strip().split("\t")
-            bo_id   = None
-
-            sbj_start = None
-            sbj_end   = None
-
-            for ref in ref_list:
-
-                ref = ref.strip()
-
-                if ref.startswith(">"):
-
-                    if id_tmp:
-                        faLen = len(seq_tmp)
-                        if sbj_start > sbj_end:
-
-                            rsbj_start = faLen - sbj_start + 1
-                            rsbj_end   = faLen - sbj_end + 1
-                            fa_tmp = seq_tmp[::-1][rsbj_start:rsbj_end]
-
-                        else:
-                            fa_tmp = ref[sbj_start:sbj_end]
-
-                        res_fa.write("%s\t%s\n" % (id_tmp, fa_tmp))
-
-                        id_tmp  = None
-                        seq_tmp = None
-
-                    elif bl_id in ref:
-                        id_tmp = bl_id
-
-
-                elif not ref.startswith(">"):
-                    try:
-                        seq_tmp += ref
-                    except Exception:
-                        seq_tmp  = ref
-
-
-
-        blastRes.close()
-
-
-    pass
-
-
-
-
-
+                # print(outLine)
 
 
 
 if __name__ == "__main__":
-
     ref_dict = reference_seqMerge(reference_path)
-    blast(ref_dict)
 
-    # bowtie(ref_dict)
+    blast(blastRes_path, ref_dict)
+    bowtie(bowtieRes_path, ref_dict)
