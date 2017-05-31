6cc2c06328cb21115a9c96ce924e34b2e85023d8
stonepy <13210700020@fudan.edu.cn>
2017-05-31 10:30:41 +0800

    hello world

---
+++ GPU_Bioinformatic/Other_tools/GTF_GFF_mutualConversion.py
@@ -0,0 +1,82 @@
+
+# _ Information ________________________________________________________________________
+
+__Name__           = """ GTF_GFF_mutualConversion """
+__Description__    = """ \n Use this tool for converting GTF and GFF mutually  \n """
+__Author__         = """ Hwx """
+__Version__        = """ 0 """
+__DevEnv__         = """ Red Hat 4.8.5-11/Ubuntu16.04 LTS;Python3.5.3,virtualenv15.1.0 """
+__FirstCreate__    = """ 2017-05-31"""
+__LastModificate__ = """ 2017-05-31"""
+__Notes__          = """ None """
+# _ Information ________________________________________________________________________
+
+
+
+# _ Packages ___________________________________________________________________________
+
+import argparse
+import os
+import sys
+import subprocess
+import multiprocessing
+
+# _ Packages ___________________________________________________________________________
+
+
+
+# _ Arguments __________________________________________________________________________
+
+
+parser = argparse.ArgumentParser(description=__Description__)
+
+parser.add_argument("arg1", help="ThisIsThePositionalArgument1", )
+parser.add_argument("-arg2", help="ThisIsTheOptionalArgument2", default=None)
+parser.add_argument("-a3", "--arg3", help="ThisIsTheOptionalArgument3", default=None)
+print()
+args = parser.parse_args()
+
+sCMD = "python %s %s %s" % (__file__, args.arg1, '-arg2 '+str(args.arg2))
+print("\n    Running command: %s\n" % sCMD)
+
+
+# _ Arguments __________________________________________________________________________
+
+
+
+# _ Main _______________________________________________________________________________
+
+class model:
+
+    def __init__(self):
+        pass
+
+
+
+# _ Main _______________________________________________________________________________
+
+
+
+# _ Execution Control __________________________________________________________________
+
+if __name__ == "__main__":
+    pass
+
+
+
+# _ Execution Control __________________________________________________________________
+
+
+
+
+
+# - Log --------------------------------------------------------------------------------
+Log = """
+
+2017-06-
+    1)
+
+
+
+"""
+# - Log --------------------------------------------------------------------------------
