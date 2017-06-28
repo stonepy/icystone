165d42afba12439de1dd606060f6c1ba02ceb0b9
stonepy <13210700020@fudan.edu.cn>
2017-06-28 16:44:02 +0800

    hello world

--- tmp_10.py
+++ tmp_10.py
@@ -1,4 +1,97 @@
 """
+    Convert 'MuTect*' result into '*library' for Annovar Annotation
+"""
+
+
+import sys
+
+
+
+try:
+    assert len(sys.argv) > 1
+    inputPath  = sys.argv[1]
+    outputPath = sys.argv[2]
+    MTtype    = sys.argv[3]
+
+except:
+    # inputPath  = "Mutect1_wjc_chr1.call_stats.txt"
+    # outputPath = "Mutect1_wjc_chr1.call_stats_READs.txt"
+    # MTtype     = "1"
+
+    print("\nUsage:\n    python  %s  <libraryPath>  <AnnovarDir>  <outputPath>" % __file__)
+    exit()
+
+
+# For MuTect1
+def MT1():
+
+    with open(inputPath, "r") as inf:
+
+        outf = open(outputPath, "w")
+        for l in inf:
+
+            if l.startswith("#"):
+                continue
+
+            l_split = l.split("\t")
+
+            chro = l_split[0]
+            pos  = l_split[1]
+            ref  = l_split[3]
+            alt  = l_split[4]
+
+            Tumor_ref_count  = l_split[25]
+            Tumor_alt_count  = l_split[26]
+            Normal_ref_count = l_split[37]
+            Normal_alt_count = l_split[38]
+
+            wl = "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" % (chro, pos, pos, ref, alt, Tumor_ref_count, Tumor_alt_count, Normal_ref_count, Normal_alt_count)
+            outf.write(wl)
+
+        outf.close()
+
+
+# For MuTect2
+def MT2():
+
+    with open(inputPath, "r") as inf:
+
+        outf = open(outputPath, "w")
+        for l in inf:
+
+            if l.startswith("#"):
+                continue
+
+            l_split = l.split("\t")
+
+            chro = l_split[0]
+            pos  = l_split[1]
+            ref  = l_split[3]
+            alt  = l_split[4]
+
+            Tumor_ref_count  = l_split[-2].split(":")[1].split(",")[0]
+            Tumor_alt_count  = l_split[-2].split(":")[1].split(",")[1]
+            Normal_ref_count = l_split[-1].split(":")[1].split(",")[0]
+            Normal_alt_count = l_split[-1].split(":")[1].split(",")[1]
+
+            wl = "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" % (chro, pos, pos, ref, alt, Tumor_ref_count, Tumor_alt_count, Normal_ref_count, Normal_alt_count)
+            outf.write(wl)
+
+        outf.close()
+
+
+
+if "1" in MTtype:
+    MT1()
+
+elif "2" in MTtype:
+    MT2()
+
+
+
+
+
+"""
    Library Annovar annotation summary
 """
 
@@ -11,12 +104,12 @@ try:
     AnnovarDir  = sys.argv[2]
     outputPath  = sys.argv[3]
 except:
-    libraryPath = "tmp/Mutect1_whole_T2N2_KEEP.call_stats.library"
-    AnnovarDir  = "tmp/Mutect1_whole_T2N2_KEEP/"
-    outputPath  = "test.xls"
+    # libraryPath = "tmp/Mutect1_whole_T2N2_KEEP.call_stats.library"
+    # AnnovarDir  = "tmp/Mutect1_whole_T2N2_KEEP/"
+    # outputPath  = "test.xls"
 
-    # print("\nUsage:\n    python  %s  <libraryPath>  <AnnovarDir>  <outputPath>" % __file__)
-    # exit()
+    print("\nUsage:\n    python  %s  <libraryPath>  <AnnovarDir>  <outputPath>" % __file__)
+    exit()
 
 summary_dict = {
 
@@ -44,7 +137,7 @@ summary_dict = {
 # Build the index
 with open(libraryPath, "r") as lib:
     lib_dict  = {}
-    lib_title = ["Chrs", "pos", "pos", "RefAllele", "AltAllele"]
+    lib_title = ["Chrs", "pos", "pos", "RefAllele", "AltAllele", "Tumor_ref_count", "Tumor_alt_count", "Normal_ref_count", "Normal_alt_count"]
 
     for l in lib:
 
@@ -140,7 +233,6 @@ OptSplit_dict = {
 }
 
 
-
 def score_pred(path, score, pred):
 
     with open(path, "r") as f:
@@ -170,7 +262,6 @@ def score_pred(path, score, pred):
 
 for key in OptSplit_dict:
     score_pred(OptSplit_dict[key]["path"], OptSplit_dict[key]["title"][0], OptSplit_dict[key]["title"][1])
-
 
 
 # Summarize the rest of annotations.
@@ -204,9 +295,6 @@ for key in summary_dict:
             lib_dict[chro][pos] += "\t"
 
 
-
-
-
 with open(outputPath, "w") as outf:
     title = ""
     for i in lib_title:
@@ -226,7 +314,7 @@ with open(outputPath, "w") as outf:
 
 
 """
-brary Annovar annotation
+    library Annovar annotation
 """
 
 from multiprocessing import Pool
@@ -236,12 +324,20 @@ import sys
 import time
 
 
-inputPath = sys.argv[1]
+try:
+    assert len(sys.argv) == 2
+    inputPath = sys.argv[1]
+
+except:
+    print("\nUsage:\n    python  %s  <libraryPath>\n" % __file__)
+    exit()
+
 print("\n %s \n" % inputPath)
 time.sleep(1)
-refDir = "/home/pub/database/Human/hg19/Annotation/"
-outputDir = ""
 
+
+# Server 6#
+refDir = "/home/pub/database/Human/hg19/Annotation/"
 
 opt_list = [
 
@@ -266,7 +362,6 @@ opt_list = [
     "-filter -dbtype gnomad_exome",
     "-filter -dbtype icgc21 -otherinfo",
 
-
 ]
 
 
@@ -282,5 +377,144 @@ for cmd in cmd_list:
     print("\n %s \n" % cmd)
     Log.write("\n %s \n" % cmd)
     P.apply_async(call(cmd, shell=True))
+P.close()
+P.join()
+
+Log.close()
+
+
+
+
+
+
+
+"""
+    Convert 'MuTect*' result into '*library' for Annovar Annotation
+"""
+
+
+import sys
+
+
+
+try:
+    assert len(sys.argv) > 1
+    inputPath  = sys.argv[1]
+    outputPath = sys.argv[2]
+    MTtype     = sys.argv[3]
+
+except:
+    inputPath  = "Mutect1_wjc_chr1.call_stats.txt"
+    outputPath = "Mutect1_wjc_chr1.call_stats.txt.library"
+    MTtype     = "1"
+
+    print("\nUsage:\n    python  %s  <MuTectResultPaht> <outputPath> <MuTectVersion>\n" % __file__)
+    exit()
+
+
+# For MuTect1
+def MT1():
+
+    with open(inputPath, "r") as inf:
+
+        outf = open(outputPath, "w")
+        for l in inf:
+
+            if l.startswith("#") or l.startswith("contig"):
+                continue
+
+            l_split = l.split("\t")
+
+            chro = l_split[0]
+            pos  = l_split[1]
+            ref  = l_split[3]
+            alt  = l_split[4]
+
+            Tumor_ref_count  = l_split[25]
+            Tumor_alt_count  = l_split[26]
+            Normal_ref_count = l_split[37]
+            Normal_alt_count = l_split[38]
+
+            wl = "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" % (chro, pos, pos, ref, alt, Tumor_ref_count, Tumor_alt_count, Normal_ref_count, Normal_alt_count)
+            outf.write(wl)
+
+        outf.close()
+
+
+# For MuTect2
+def MT2():
+
+    with open(inputPath, "r") as inf:
+
+        outf = open(outputPath, "w")
+        for l in inf:
+
+            if l.startswith("#"):
+                continue
+
+            l_split = l.split("\t")
+
+            chro = l_split[0]
+            pos  = l_split[1]
+            ref  = l_split[3]
+            alt  = l_split[4]
+
+            Tumor_ref_count  = l_split[-2].split(":")[1].split(",")[0]
+            Tumor_alt_count  = l_split[-2].split(":")[1].split(",")[1]
+            Normal_ref_count = l_split[-1].split(":")[1].split(",")[0]
+            Normal_alt_count = l_split[-1].split(":")[1].split(",")[1]
+
+            wl = "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" % (chro, pos, pos, ref, alt, Tumor_ref_count, Tumor_alt_count, Normal_ref_count, Normal_alt_count)
+            outf.write(wl)
+
+        outf.close()
+
+
+if "1" in MTtype:
+    MT1()
+
+elif "2" in MTtype:
+    MT2()
+
+
+
+
+
+
+"""
+    Samtools tview
+"""
+
+from multiprocessing import Pool
+from subprocess import call
+
+import sys
+
+
+SNV_Path  = sys.argv[1]
+BAM_Path  = sys.argv[2]
+outputDir = sys.argv[3]
+
+refPATH = "/home/hwx/DevPipline/Tumor_SNP_Hwx/Database/hg19/hg19.fa"    # Server 2#
+
+
+with open(SNV_Path, "r") as inf:
+
+    site_list = {}
+    cmd_list  = []
+    for l in inf:
+        l_split = l.split("\t")
+        pos = "%s:%s" % (l_split[0], l_split[1])
+
+        # If the file name does not start with letter 't', it won't work properly
+        output_Path = outputDir + "/tchr%s_%s.tview" % (l_split[0], l_split[1])
+        cmd = "/home/hwx/DevPipline/Tumor_SNP_Hwx/Software/samtools-1.4.1/samtools tview -d t -p {pos} {BAM} {ref} > {output}".format(pos=pos, BAM=BAM_Path, ref=refPATH, output=output_Path)
+        # print("\n %s \n" % cmd)
+        cmd_list.append(cmd)
+
+P = Pool(15)
+for cmd in cmd_list:
+    P.apply_async(call(cmd, shell=True))
+    print(cmd)
 P.close()
 P.join()
